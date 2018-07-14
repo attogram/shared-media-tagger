608f3da9cfafdf9895c6a8f15b7dd15faf0251f2
Attogram Project <attogram@users.noreply.github.com>
2018-07-14 16:46:24 +0200

    fixes

--- src/Template/TagBar.php
+++ src/Template/TagBar.php
@@ -11,11 +11,8 @@ declare(strict_types = 1);
 <div style="font-size:270%;">
 <?php
 foreach ($this->tags as $tag) {
-    ?><div class="d-inline"><a
-            class="pl-1 pr-1"
-            href="<?= $tag['link'] ?>"
-            title="<?= $tag['name'] ?>"><?= $tag['display_name'] ?></a
-            class="pl-1 pr-1"></div><?php
+    ?><div class="d-inline"><a class="pl-1 pr-1" href="<?= $tag['link'] ?>"
+        title="<?= $tag['name'] ?>"><?= $tag['display_name'] ?></a></div><?php
 }
 ?>
 </div>
