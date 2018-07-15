b4445c93fc039d93226392912a10f11270f51a26
Attogram Project <attogram@users.noreply.github.com>
2018-07-16 00:17:49 +0200

    v1.1.3

--- src/Loader.php
+++ src/Loader.php
@@ -25,7 +25,7 @@ class Loader
      */
     public function __construct(array $config = [])
     {
-        define('SHARED_MEDIA_TAGGER', '1.1.2');
+        define('SHARED_MEDIA_TAGGER', '1.1.3');
 
         ob_start('ob_gzhandler');
 
