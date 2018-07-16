288b924cd8adf74284d3491f6279224dbe1f0b44
Attogram Project <attogram@users.noreply.github.com>
2018-07-16 23:57:20 +0200

    pagination

--- src/View/Scores.php
+++ src/View/Scores.php
@@ -1,13 +1,11 @@
 <?php
-declare(strict_types = 1);
 /**
  * Shared Media Tagger
  * Scores View
  *
  * @var array $data
  */
-
-use Attogram\SharedMedia\Tagger\Tools;
+declare(strict_types = 1);
 
 ?>
 <div class="row bg-white">
@@ -18,43 +16,7 @@ use Attogram\SharedMedia\Tagger\Tools;
             with <?= $this->smt->database->getTotalVotesCount() ?> scores total,
             on a scale of 1 to 5 / # of votes
         </small>
-
-        <?php if ($data['pages'] > 1) { ?>
-        <nav aria-label="Scores Pagination">
-            <ul class="pagination pagination-sm justify-content-center flex-wrap">
-                <?php if ($data['page'] > 1) { ?>
-                <li class="page-item">
-                    <a class="page-link" href="<?=
-                    Tools::url('scores') . '/' . ($data['page'] - 1)
-                    ?>" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-                <?php } ?>
-                <?php for ($page = 1; $page <= $data['pages']; $page++) {
-                    $classActive = '';
-                    if ($page === $data['page']) {
-                        $classActive = ' active';
-                    }
-                    ?>
-                    <li class="page-item<?= $classActive ?>"><a class="page-link" href="<?=
-                           Tools::url('scores') . ($page > 1 ? '/' . $page : '')
-                        ?>"><?= $page ?></a></li>
-                <?php } ?>
-                <?php if ($data['page'] < $data['pages']) { ?>
-                <li class="page-item">
-                    <a class="page-link" href="<?=
-                    Tools::url('scores') . '/' . ($data['page'] + 1)
-                    ?>" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-                <?php } ?>
-            </ul>
-        </nav>
-        <?php } ?>
+        <?php $this->printPagination($data); ?>
     </div>
 </div>
 <div class="row bg-white">
@@ -68,5 +30,10 @@ use Attogram\SharedMedia\Tagger\Tools;
             <?php $this->smt->includeThumbnailBox($media); ?>
         </div>
         <?php } ?>
+    </div>
+</div>
+<div class="row bg-white">
+    <div class="col-12 mb-3">
+        <?php $this->printPagination($data); ?>
     </div>
 </div>
