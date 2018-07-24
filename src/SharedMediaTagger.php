23d6b6e3bb0c47a7fa0f9cbf2c33b50bf87b6320
Attogram Project <attogram@users.noreply.github.com>
2018-07-24 22:34:37 +0200

    v1.1.8

--- src/SharedMediaTagger.php
+++ src/SharedMediaTagger.php
@@ -22,7 +22,7 @@ class SharedMediaTagger
      */
     public function __construct(array $config = [])
     {
-        define('SHARED_MEDIA_TAGGER', '1.1.7');
+        define('SHARED_MEDIA_TAGGER', '1.1.8');
 
         ob_start('ob_gzhandler');
 
